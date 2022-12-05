ef56d48580eb22bf791971fb7fb9cf31d50640a8
winkingcat <dankercher@gmail.com>
2022-12-05 15:56:31 -0500

    add authentication forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Logged in successfully"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Login unsuccessful"
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
+  end
 end
