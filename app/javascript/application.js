2c60cc619bd7ce9b16ca43c29bb51ecf1e511e0e
winkingcat <dankercher@gmail.com>
2022-12-05 12:55:45 -0500

    add first pages, dropdown, and favicon

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import * as jquery from "jquery"
-import "semantic-ui" 
+import "@hotwired/turbo-rails";
+import "controllers";
+import * as jquery from "jquery";
+import "semantic-ui";
+
+$(document).on("turbo:load", function () {
+  $(".ui.dropdown").dropdown();
+});
